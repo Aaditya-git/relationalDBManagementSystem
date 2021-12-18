ac8a1c0bdf0766f95fdb28a3c7ee815330462afc
Aaditya-git <aadityab134@gmail.com>
2021-12-18 18:18:35 +0530

    Commited

--- CICD/runServer.sh
+++ CICD/runServer.sh
@@ -1,12 +1,15 @@
 WORKSPACE=$1
 DESTINATION="/var/opt/rdbms/relationalDBManagementSystem/"
+SITE_PACKAGES_PATH="/lib/python3.7/site-packages"
 sudo rm -rf $DESTINATION
 sudo mkdir -p $DESTINATION
 
 sudo cp -rvf $WORKSPACE/* $DESTINATION
-sudo chown -R $USER:$USER /usr/local/lib/python3.7/site-packages
-echo "/var/opt/rdbms/relationalDBManagementSystem" > /usr/local/lib/python3.7/site-packages/addProjectPath.pth
-ls -lrt /usr/local/lib/python3.7/site-packages/
+sudo chown -R $USER:$USER $SITE_PACKAGES_PATH
+echo $DESTINATION > $SITE_PACKAGES_PATH/addProjectPath.pth
+# ls -lrt /usr/local/lib/python3.7/site-packages/
+ls -lrt $SITE_PACKAGES_PATH
+
 echo "Starting Server..."
 cd $DESTINATION/rdbms/
 pwd
