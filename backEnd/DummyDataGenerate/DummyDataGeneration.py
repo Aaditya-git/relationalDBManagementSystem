002adef7cd216c9c23dc3847884b5fe945d094ee
Aaditya Bhilegaonkar <69500186+Aaditya-git@users.noreply.github.com>
2021-12-18 19:19:09 +0530

    Update DummyDataGeneration.py

--- backEnd/DummyDataGenerate/DummyDataGeneration.py
+++ backEnd/DummyDataGenerate/DummyDataGeneration.py
@@ -13,7 +13,7 @@ sys.path.append("C:\\Users\\lenovo\\data structure in python\\BE project\\relati
 sys.path.append("C:\\Users\\lenovo\\data structure in python\\BE project\\relationalDBManagementSystem\\backEnd\\DummyDataGenerate")
 
 # Importing User defined Modules
-from sqlConnector import *
+from sqlConnector import executeGetCommand, executeInsertCommand
 from dummyDataPayload import *
 # from encryDecryFn import fillEnctryptedValues
 from utility import *
